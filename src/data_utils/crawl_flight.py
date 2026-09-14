e19e42dcef10929541bf14836b351fe19ad7b2e7
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-09-14 12:51:47 +0000

    Add data_utils files

---
+++ src/data_utils/crawl_flight.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv("../.env")
+
+aviation_api= os.getenv("AVIATION_API_KEY")
+aviation_url= "https://api.aviationstack.com/v1/flights"
+
+
+def get_data(url, api):
+    params= {
+        "access_key": api
+    }
+    r= requests.get(url, params= params)
+    return r.json()
+
+if __name__ == "__main__":
+    flight_data= get_data(aviation_url, aviation_api)
+    with open("data/flight_data.json", "w") as f:
+        json.dump(flight_data, f)
+    
---
+++ src/data_utils/process_data.py
@@ -0,0 +1,50 @@
+import json
+
+processed= list()
+
+
+if __name__ == "__main__":
+    with open("data/flight_data.json", "r") as f:
+        flight_data= json.load(f)
+
+    with open("data/meteo_data.json", "r") as m:
+        meteo_data= json.load(m)
+
+    for flight in flight_data["data"]:
+        combined= {
+            "arrival": dict(),
+            "departure": dict(),
+        }
+        combined["flight_date"]= flight["flight_date"]
+        combined["flight_status"]= flight["flight_status"]
+
+        combined["arrival"]["airport"]= flight["arrival"]["airport"]
+        combined["arrival"]["timezone"]= flight["arrival"]["timezone"]
+        combined["arrival"]["iata"]= flight["arrival"]["iata"]
+        combined["arrival"]["scheduled"]= flight["arrival"]["scheduled"]
+        combined["arrival"]["delay"]= flight["arrival"]["delay"]
+
+        combined["departure"]["airport"]= flight["departure"]["airport"]
+        combined["departure"]["timezone"]= flight["departure"]["timezone"]
+        combined["departure"]["iata"]= flight["departure"]["iata"]
+        combined["departure"]["scheduled"]= flight["departure"]["scheduled"]
+        combined["departure"]["delay"]= flight["departure"]["delay"]
+
+        processed.append(combined)
+
+    for meteo in meteo_data:
+        for c in processed:
+            if c["arrival"]["iata"] == meteo["iata"]:
+                c["arrival"]["data"] = meteo["meteo_data"]
+            elif c["departure"]["iata"] == meteo["iata"]:
+                c["departure"]["data"] = meteo["meteo_data"]
+            else:
+                continue
+    
+    with open("data/processed.json", "w") as f:
+        json.dump(processed, f)
+
+
+
+
+    
